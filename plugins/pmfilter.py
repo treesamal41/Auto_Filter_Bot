61fa5c78cccf4c65b943a4814012695472e01f22
treesamal41 <treesamal41@gmail.com>
2026-09-22 08:17:34 +0530

    Add files via upload

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1650,6 +1650,8 @@ async def auto_filter(client, msg, spoll=False):
 
                     for idx, file in enumerate(files, start=1):
                         cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
+        if imdb and imdb.get("imdb_id"):
+            btn.append([InlineKeyboardButton("📝 sᴜʙᴛɪᴛʟᴇs", callback_data=f"subdl#{imdb['imdb_id']}")])
         sent = None
         try:
             if imdb and imdb.get('poster'):
