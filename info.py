de7357b992a3570cd8681e026bfa016ed286ead3
treesamal41 <treesamal41@gmail.com>
2026-09-24 04:51:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1002134939744")# Channels for
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')    # QR code image for payments
+QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/9zhwqp.jpg`')    # QR code image for payments
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
